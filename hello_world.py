050dad7a0c543fbadf8ed2cf3cd3e73afac30298
yunadada <ynson0216@naver.com>
2023-09-13 15:18:25 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,11 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+print("Kumoh National Institute of Technology.")
+
+
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
     
